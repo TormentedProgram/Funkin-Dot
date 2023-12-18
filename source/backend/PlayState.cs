a6d285c1c7247f025b43b3a2807e3430144d4f67
TormentedProgram <69302186+TormentedProgram@users.noreply.github.com>
2023-12-18 18:08:46 +1100

    2342d

--- source/backend/PlayState.cs
+++ source/backend/PlayState.cs
@@ -6,6 +6,7 @@ public partial class PlayState : MusicBeatState
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        Input.MouseMode = Input.MouseModeEnum.Hidden;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
